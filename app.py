97840756c667f61e54fb9f3b7792ecb0bbd20c34
Rachit Jain <jainrachit124@gmail.com>
2023-05-10 08:30:29 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def app():
     height = st.number_input("Height (in cm)", min_value=0, max_value=None, value=100)
     width = st.number_input("Width (in cm)", min_value=0, max_value=None, value=50)
     weight = st.number_input("Weight (in kg)", min_value=0, max_value=None, value = 10)
-    material = st.selectbox("Material", ["Wood", "Brass", "Aluminium", "Bronze", "Marble"])
+    material = st.selectbox("Material", ["Wood", "Brass", "Aluminium", "Bronze", "Stone","Clay", "Marble"])
     base_price = st.number_input("Base Price (in USD)", min_value=0, max_value=None, value=200)
     price = st.number_input("Price (in USD)", min_value=0, max_value=None, value=250)
     international = st.selectbox("International", ["Yes", "No"])
@@ -18,7 +18,7 @@ def app():
     installation_included = st.selectbox("Installation Included", ["Yes", "No"])
     transportation = st.selectbox("Transportation Included", ["Yes", "No"])
     fragile = st.selectbox("Fragile", ["Yes", "No"])
-    customer_info = st.selectbox("Customer Information", ["Wealthy", "Working"])
+    customer_info = st.selectbox("Customer Information", ["Wealthy", "Working Class"])
     remote_location = st.selectbox("Remote Location", ["Yes", "No"])
 
     if st.button("Predict Shipping Cost"):
